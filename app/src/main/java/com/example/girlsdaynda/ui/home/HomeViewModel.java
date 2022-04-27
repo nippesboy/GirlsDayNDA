7be1d5fa4c3b49d0c5341f46b043f890372b0c17
u1302 <u1302@ford.com>
2022-04-27 10:50:10 +0200

    first test of user u1302

--- app/src/main/java/com/example/girlsdaynda/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/girlsdaynda/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is home fragment of user U1302");
     }
 
     public LiveData<String> getText() {
