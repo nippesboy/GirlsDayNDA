09a55c687c0873ac76ee018dfa04496bb9399352
Cem Akbaba <cakbaba@ford.com>
2022-04-27 17:45:11 +0200

    added activity

--- app/src/main/java/com/example/girlsdaynda/MainActivity.java
+++ app/src/main/java/com/example/girlsdaynda/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.girlsdaynda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
--- app/src/main/java/com/example/girlsdaynda/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/girlsdaynda/ui/gallery/GalleryFragment.java
@@ -1,9 +1,12 @@
 package com.example.girlsdaynda.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,24 +15,39 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.girlsdaynda.DisplayMessageActivity;
 import com.example.girlsdaynda.R;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment implements View.OnClickListener {
 
     private GalleryViewModel galleryViewModel;
+    View root;
+    public static final String EXTRA_MESSAGE = "com.example.girlsdaynda.ui.gallery.MESSAGE";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
+        root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        final TextView textView = root.findViewById(R.id.editTextTextPersonName);
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+        Button button = (Button) root.findViewById(R.id.button);
+        button.setOnClickListener(this);
         return root;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getActivity(), DisplayMessageActivity.class);
+        EditText editText = (EditText) root.findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
